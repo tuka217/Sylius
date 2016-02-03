51834c9ba8217fd6cc4ffc58c406fc86b7a1bb17
tuka217 <a.walasek@gazeta.pl>
2016-02-03 09:09:36 +0100

    [ResourceBundle] Create integration tests [ResourceBundle] Add SyliusResourceExtensionTest with test about loading configuration for new entity [ResourceBundle] Add configuration test

---
+++ src/Sylius/Bundle/ResourceBundle/Tests/Functional/app/autoload.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$loader = require __DIR__.'/../../../vendor/autoload.php';
+
+require __DIR__.'/AppKernel.php';
---
+++ src/Sylius/Bundle/ResourceBundle/Tests/Functional/src/AppBundle/Entity/Book.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity;
+
+/**
+ * @author Anna Walasek <anna.walasek@lakion.com>
+ */
+class Book
+{
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+}
